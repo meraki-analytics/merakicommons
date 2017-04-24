8c254c938b3869703bd3dc06488d07c245ae1141
Rob Rua <robertrua@gmail.com>
2017-04-23 21:41:07 -0400

    test lazy property w/multiple object instances

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -1,17 +1,19 @@
 from merakicommons.cache import lazy_property
 
-TEST_VALUE = "TEST VALUE"
+TEST_VALUE_1 = "TEST VALUE_1"
+TEST_VALUE_2 = "TEST_VALUE_2"
 VALUE_COUNT = 100
 
 
 class LazyProperty(object):
-    def __init__(self) -> None:
+    def __init__(self, value) -> None:
         self.property_calls = 0
+        self.value = value
 
     @lazy_property
     def property(self) -> str:
         self.property_calls += 1
-        return TEST_VALUE
+        return self.value
 
 
 def test_lazy_property_type():
@@ -19,16 +21,25 @@ def test_lazy_property_type():
 
 
 def test_lazy_property_value():
-    x = LazyProperty()
+    x = LazyProperty(TEST_VALUE_1)
+    y = LazyProperty(TEST_VALUE_2)
     for _ in range(VALUE_COUNT):
         value = x.property
-        assert type(value) is type(TEST_VALUE)
-        assert value == TEST_VALUE
+        assert type(value) is type(TEST_VALUE_1)
+        assert value == TEST_VALUE_1
+
+        value = y.property
+        assert type(value) is type(TEST_VALUE_2)
+        assert value == TEST_VALUE_2
 
 
 def test_lazy_property_loading():
-    x = LazyProperty()
+    x = LazyProperty(TEST_VALUE_1)
+    y = LazyProperty(TEST_VALUE_2)
     assert x.property_calls == 0
+    assert y.property_calls == 0
     for _ in range(VALUE_COUNT):
         x.property
+        y.property
         assert x.property_calls == 1
+        assert y.property_calls == 1
