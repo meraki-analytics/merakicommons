d4e6d4d06bbad59bfad5459dfaf98f13ac342542
jjmaldonis <jjmaldonis@gmail.com>
2017-07-25 23:14:32 -0400

    the cache now caches objects by type

--- merakicommons/cache.py
+++ merakicommons/cache.py
@@ -2,6 +2,7 @@ from functools import wraps
 from typing import Callable, Any, TypeVar
 from weakref import WeakKeyDictionary
 from threading import Lock
+from collections import defaultdict
 
 T = TypeVar("T")
 
@@ -38,33 +39,24 @@ def lazy_property(method: Callable[[Any], T]) -> property:
 
 class _CacheSegment(object):
     def __init__(self) -> None:
-        self._data = {}
+        self._data = defaultdict(dict)
         self._lock = Lock()
 
-    def put(self, key: Any, value: Any, timeout: int = -1) -> None:
+    def put(self, type: Any, key: Any, value: Any, timeout: int = -1) -> None:
         with self._lock:
-            self._data[key] = value
+            self._data[type][key] = value
 
-    def get(self, key: Any) -> Any:
+    def get(self, type: Any, key: Any) -> Any:
         with self._lock:
-            return self._data[key]
+            return self._data[type][key]
 
-    def delete(self, key: Any) -> None:
+    def delete(self, type: Any, key: Any) -> None:
         with self._lock:
-            del self._data[key]
+            del self._data[type][key]
 
     def __len__(self) -> int:
         with self._lock:
             return self._data.__len__()
-
-    def __getitem__(self, key: Any) -> Any:
-        return self.get(key)
-
-    def __setitem__(self, key: Any, value: Any) -> None:
-        self.put(key, value)
-
-    def __delitem__(self, key: Any) -> None:
-        self.delete(key)
 
     def __contains__(self, item: Any) -> bool:
         with self._lock:
